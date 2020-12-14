708e4b965796bc901f60bf34da29ca7d27a2d4e7
Azhar Rais <dev@azharaiz.com>
2020-12-14 14:23:39 +0700

    Implement lab 7

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/labs/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/labs/MainActivity.java
@@ -3,12 +3,30 @@ package id.ac.ui.cs.mobileprogramming.azharaiz.labs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView name;
+    EditText inputName;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.textView);
+        inputName = findViewById(R.id.editTextTextPersonName);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name.setText(inputName.getText().toString());
+            }
+        });
     }
 }
